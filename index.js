e4838fc9439d2ae80c4c557f9ff62b9a4c2b2476
Eric Schoffstall <contra@wearefractal.com>
2014-01-14 16:38:15 -0700

    initial impl

--- index.js
+++ index.js
@@ -8,12 +8,29 @@ module.exports = function (doit, child) {
 	if (!child) {
 		throw new Error('gulp-if: child action is required');
 	}
-	if (doit) {
-		return child;
-	} else {
-		// noop
-		return through(function(data){
-			this.emit('data', data);
-		});
-	}
+	// when we get a file:
+	// if it passes we write it back to ourselves via the child that is piped to us
+	// we attach a field to it to not run the truth test again
+	var process = function(file) {
+		if (file._ignore) {
+			delete file._ignore;
+			this.queue(file);
+			return;
+		}
+
+		if (typeof doit === 'function') {
+			if (doit(file)) {
+				file._ignore = true;
+				child.write(file);
+			}
+			return
+		}
+		if (typeof doit ==='boolean' && doit) {
+			file._ignore = true;
+			child.write(file);
+		}
+	};
+	var outStream = through(process);
+	child.pipe(outStream);
+	return outStream;
 };
